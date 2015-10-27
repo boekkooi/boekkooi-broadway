8616af4601097de1d6361928200ef71b524529d3
Warnar Boekkooi <warnar@cerbex.com>
2015-10-27 12:37:21 +0800

    Added CommandHandler that uses tactician

---
+++ src/CommandHandling/CommandHandler.php
@@ -0,0 +1,9 @@
+<?php
+namespace Boekkooi\Broadway;
+
+use Broadway\CommandHandling\CommandHandlerInterface;
+use League\Tactician\CommandBus;
+
+class CommandHandler extends CommandBus implements CommandHandlerInterface
+{
+}
